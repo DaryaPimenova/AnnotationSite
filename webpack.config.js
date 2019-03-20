bb3bb340c9cc66c3fb3e496aa847633b7682221b
vladislavvasyukov <vvasiukov@dev.kokoc.com>
2019-03-20 10:24:16 +0300

    Добавил сжатие css при билде

--- webpack.config.js
+++ webpack.config.js
@@ -3,46 +3,11 @@ var webpack = require('webpack');
 var colors = require('colors/safe');
 var glob = require("glob");
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var BitBarWebpackProgressPlugin = require("bitbar-webpack-progress-plugin");
 var fs = require('fs');
 
 require('es6-promise')
     .polyfill();
-
-entries = [];
-entiesCount = 1;
-
-function fillEntries(entry) {
-    entry = "./" + entry;
-    console.log(colors.green("    " + entiesCount + ": " + entry));
-    entries.push(entry);
-    entiesCount += 1;
-}
-
-rmDir = function (dirPath) {
-    try {
-        var files = fs.readdirSync(dirPath);
-    } catch (e) {
-        return;
-    }
-    if (files.length > 0)
-        for (var i = 0; i < files.length; i++) {
-            var filePath = dirPath + '/' + files[i];
-            if (fs.statSync(filePath).isFile())
-                fs.unlinkSync(filePath);
-            else
-                rmDir(filePath);
-        }
-};
-
-console.log(colors.yellow('Find entries'));
-glob.sync("*/static/**/main.jsx", {
-    cwd: path.resolve(__dirname)
-})
-    .forEach(fillEntries);
-glob.sync("*/static/**/main.js", {
-    cwd: path.resolve(__dirname)
-})
-    .forEach(fillEntries);
 
 var PROD = JSON.parse(process.env.NODE_ENV || '0');
 
@@ -50,9 +15,12 @@ var plugins = [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
         'process.env': {
-            'NODE_ENV': PROD === "1" ? '"production"' : '"development"'
+            'NODE_ENV': PROD == "1" ? '"production"' : '"development"'
         }
     }),
+    new BitBarWebpackProgressPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NoErrorsPlugin(),
     new webpack.ProvidePlugin({
         '_': 'lodash',
         'moment': 'moment',
@@ -60,88 +28,87 @@ var plugins = [
         'jQuery': 'jquery',
         'swal': 'sweetalert2'
     }),
-    function () {
-        this.plugin('watch-run', function (watching, callback) {
+    function() {
+        this.plugin('watch-run', function(watching, callback) {
             console.log(colors.yellow('Begin compile at ' + new Date()));
             callback();
         })
     },
     new ExtractTextPlugin({
-        filename: (getPath) => {
+        filename: function(getPath) {
             return getPath('styles.css')
         },
         allChunks: true
     }),
 ];
 
-var watch = PROD !== 1;
+var watch = true;
 
+if (PROD) {
+    watch = false;
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        comments: false,
+        sourceMap: true,
+        compress: {
+            sequences     : true,
+            booleans      : true,
+            loops         : true,
+            unused      : true,
+            warnings    : false,
+            drop_console: true,
+            unsafe      : true
+        }
+    }));
+}
 console.log(colors.yellow('Run building'));
 
-targets = [];
 
-console.log(watch);
-console.log(entries);
-entries.forEach(function (entry) {
-    var context = path.resolve(path.dirname(entry));
-    var buildFolder = path.join(context, 'build');
-
-    rmDir(buildFolder);
-
-    var targetPlugins = plugins;
-
-    targets.push({
+module.exports = {
         cache: true,
-        context: path.resolve(path.dirname(entry)),
-        entry: './main.js',
-        optimization: {
-            minimize: !watch,
-        },
-        mode: PROD === 1 ? "production" : "development",
+        context: path.join(path.resolve(__dirname), "core", "static", "core"),
+        entry: "main.js",
         output: {
-            path: buildFolder,
+            path: path.join(path.resolve(__dirname), "core", "static", "core", "build"),
             filename: "app.build.js",
         },
-        plugins: targetPlugins,
+        plugins: plugins,
         devtool: "cheap-module-source-map",
         module: {
-            rules: [{
+            loaders: [{
                 test: /\.(jsx|js)$/,
                 exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: "babel-loader?cacheDirectory=true",
-                    options: {
-                        presets: ["@babel/preset-react"],
-                        plugins: ["@babel/plugin-proposal-class-properties"]
-                    },
-                },
+                loader: "babel-loader",
+                options: {
+                  presets: ['es2015', "stage-0", "react"]
+                }
             }, {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract([
-                    {
-                        loader: 'css-loader'
-                    }, {
-                        loader: 'less-loader'
-                    }
-                ])
+                    test: /\.less$/,
+                    loader: ExtractTextPlugin.extract([
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                minimize: PROD,
+                            }
+                        }, {
+                            loader: 'less-loader'
+                        }
+                    ])
             }, {
                 test: /\.css$/,
-                use: ExtractTextPlugin.extract('css-loader')
+                loader: ExtractTextPlugin.extract('css-loader?minimize=' + PROD)
             }, {
-                test: /\.(jpe?g|png|gif|svg|eot|woff|woff2|ttf|otf)(\?\S*)?$/,
-                use: {loader: "file-loader?mimetype=image/svg+xml"}
+                test: /\.(jpe?g|png|gif|svg|eot|woff|ttf|svg|woff2)(\?\S*)?$/,
+                loader: "file-loader?mimetype=image/svg+xml"
             }]
         },
         watch: watch,
         resolve: {
-            modules: ['node_modules', path.resolve(path.dirname(entry))],
+            modules: ['node_modules', "./core/static/core"],
             extensions: [
                 '.jsx',
                 '.js',
             ],
             unsafeCache: true,
         }
-    })
-});
-
-module.exports = targets;
+    }
