d12e26a16debe219bb8456736067b2898a1a1652
vladislavvasyukov <v.vasiukov@dev.kokoc.com>
2019-03-21 07:19:10 +0300

    Поставил пакет react-image-crop

--- core/static/core/components/Annotation.js
+++ core/static/core/components/Annotation.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
 import Menu from './Menu';
 import {connect} from 'react-redux';
 import {annotation, auth} from "../actions";
 
 
 class Annotation extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            crop: {
+                x: 20,
+                y: 10,
+                width: 300,
+                height: 100
+            }
+        };
+    }
+
+    onChange = (crop) => {
+        this.setState({ crop });
+    }
+
 
     render() {
         const { isAuthenticated, logout } = this.props;
@@ -13,6 +32,7 @@ class Annotation extends React.Component {
         <div>
             <Menu isAuthenticated={isAuthenticated} logout={logout} />
             <h1>Здесь будет отображаться картинка + форма для пометок</h1>
+            <ReactCrop src="/home/vladislav/Desktop/tmp.jpeg" crop={this.state.crop} onChange={this.onChange}/>
         </div>
         )
     }
