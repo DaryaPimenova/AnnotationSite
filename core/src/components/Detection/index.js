02d0507aa0a084ccaadcd03123754c9616f4d06c
vladislavvasyukov <v.vasiukov@dev.kokoc.com>
2019-07-19 23:28:44 +0300

    Инструкции

--- core/src/components/Detection/index.js
+++ core/src/components/Detection/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import Help from './Help';
+import Help from '../Help';
 import Detect from "./Detect";
+
+
+const DetectHelp = () => {
+    return (
+        <Help 
+            header="Детекция" 
+            description="Чтобы научить ИИ обнаруживать объекты на изображениях, мы предоставляем bounding box. Выберите класс из списка и выделите соответствующий объект с помощью ограничительной рамки." 
+            video_url="/assets/poster.png"
+            poster="/assets/poster.png"
+            tool_url="/detection"
+        />
+    );
+}
 
 
 export default class Detection extends React.Component {
@@ -11,7 +24,7 @@ export default class Detection extends React.Component {
             <div>
                 <Switch>
                     <Route exact path="/detection" component={Detect} />
-                    <Route exact path="/detection/help" component={Help} />
+                    <Route exact path="/detection/help" component={DetectHelp} />
                 </Switch>
             </div>
         )
